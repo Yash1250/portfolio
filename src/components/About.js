20586e8a987c54ad21acb4c17a710c94a7a91092
Yash Mehta <mehtayashkuleri503@gmail.com>
2024-05-27 00:47:49 +0530

    project completed

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <div
       name="about"
       className="bg-gradient-to-b from-gray-800  to-black w-full text-white">
-      <div className="max-w-screen-lg mx-auto flex flex-col justify-center px-4 w-full py-10 sm:py-28">
+      <div className="max-w-screen-lg mx-auto flex flex-col justify-center px-4 w-full py-16 sm:py-28">
         <div className="pb-16 mt-20 sm:mt-0 md:mt-20">
           <h2 className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             About
@@ -15,24 +15,22 @@ const About = () => {
         </div>
         <p className="text-xl">
           I am a motivated and enthusiastic front end web developer with a solid
-          foundation in ReactJS, JavaScript, CSS, HTML, Tailwind CSS and Redux
-          Toolkit. Despite being new to the professional landscape, I bring
-          fresh perspectives and a willingness to learn and adapt quickly. My
-          proficiency in these technologies enables me to hit the ground running
-          and contribute effectively to your team's projects. I am eager to
-          leverage my skills and grow alongside your organization, adding value
-          and driving success.
+          foundation in ReactJS, JavaScript, C++, HTML, CSS, Tailwind CSS, Redux
+          Toolkit, and NodeJs. Despite being new to the professional landscape,
+          I bring fresh perspectives and a willingness to learn and adapt
+          quickly. My proficiency in these technologies enables me to hit the
+          ground running and contribute effectively to various projects.
         </p>
         <br />
-        <p className="text-xl hidden flex-col sm:flex">
-          I am a motivated and enthusiastic front end web developer with a solid
-          foundation in ReactJS, JavaScript, CSS, HTML, Tailwind CSS and Redux
-          Toolkit. Despite being new to the professional landscape, I bring
-          fresh perspectives and a willingness to learn and adapt quickly. My
-          proficiency in these technologies enables me to hit the ground running
-          and contribute effectively to your team's projects. I am eager to
-          leverage my skills and grow alongside your organization, adding value
-          and driving success.
+        <p className="text-xl">
+          I have solved over 200 problems on LeetCode, demonstrating my strong
+          problem-solving skills and dedication to continuous learning. I have a
+          passion for creating user-friendly and visually appealing web
+          applications and am keen on keeping up with the latest industry trends
+          and best practices to ensure my work remains modern and efficient.
+          Additionally, I enjoy collaborating with cross-functional teams to
+          achieve common goals. I am eager to leverage my skills, grow, add
+          value, and drive success.
         </p>
       </div>
     </div>
